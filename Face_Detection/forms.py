a3b1607cfe10c72e6a679d62e19c94788b15678e
rmuditya <rmuditya@gmail.com>
2021-01-04 17:55:45 +0530

    register and login working by no redirect to login page

---
+++ Face_Detection/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import *
+
+class ResgistrationForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = [
+            'face_id',
+            'name',
+            'email',
+            'phone',
+            'image'
+        ]
